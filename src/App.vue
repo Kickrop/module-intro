f4fc77f89c212f5c5f0b38d0586d286258a4812b
den <kickrop@gmail.com>
2018-12-03 21:57:13 +0300

    dynamic components

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,20 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <app-quote>
+                <button @click="selectComponent = 'appQuote'">Quote</button>
+                <button @click="selectComponent = 'appAuthor'">Author</button>
+                <button @click="selectComponent = 'appNew'">New</button>
+               <hr>
+               <p>{{ selectComponent }}</p>
+               <keep-alive>
+                    <component :is="selectComponent">
+                        <p>Default Content</p>
+                    </component>
+               </keep-alive>
+               <!-- <app-quote>
                    <h2 slot="title">{{ quoteTitle }}</h2>
                    <p slot="content">A wonderful quote</p>
-               </app-quote>
+               </app-quote> -->
             </div>
         </div>
     </div>
@@ -13,17 +23,20 @@
 
 <script>
     import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
     export default {
         data: function() {
             return {
-                quoteTitle: 'The Quote'
+                quoteTitle: 'The Quote',
+                selectComponent: 'appQuote'
             }
         },
         components: {
             appQuote: Quote,
-            appAuthor,
-            appNew
+            appAuthor: Author,
+            appNew: New
         }
     }
 </script>
--- src/components/New.vue
+++ src/components/New.vue
@@ -1,12 +1,27 @@
 <template>
     <div>
         <h3>The Qoute</h3>
+        <button @click="counter++">increase!</button>
+        <p>{{ counter }}</p>
     </div>
 </template>
 
 <script>
 export default {
-
+    data: function () {
+        return {
+            counter: 0
+        };
+    },
+    destroyed() {
+        console.log('Destroyed!');
+    },
+    deactivated() {
+        console.log('Deactivated');
+    },
+    activated() {
+        console.log('Actevated');
+    }
 }
 </script>
 
